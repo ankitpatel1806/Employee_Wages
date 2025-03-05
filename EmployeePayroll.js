e168d1154a898c192a8836cdaa2aa2023f902592
Ankit Patel <yugap1806@gmail.com>
2025-03-05 05:53:17 +0530

    Ankit Patel Added UC13

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,16 +1,20 @@
 class EmployeePayroll {
-    constructor(id, name, salary) {
+    constructor(id, name, salary, gender, startDate) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = new Date(startDate);
     }
 
-    getDetails = () => `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}`;
+   
+    getDetails = () => 
+        `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate.toDateString()}`;
 }
 
-const emp1 = new EmployeePayroll(101, "Anand Soni", 5000);
-const emp2 = new EmployeePayroll(102, "Ankit Kumar", 7000);
-const emp3 = new EmployeePayroll(103, "Ankit Patel", 9000);
+const emp1 = new EmployeePayroll(101, "Anand Soni", 5000, "Male", "2023-05-10");
+const emp2 = new EmployeePayroll(102, "Muskan Gupta", 7000, "Female", "2023-06-10");
+const emp3 = new EmployeePayroll(103, "Ankit Patel", 9000, "Male", "2023-07-10");
 
 console.log(emp1.getDetails());
 console.log(emp2.getDetails());
